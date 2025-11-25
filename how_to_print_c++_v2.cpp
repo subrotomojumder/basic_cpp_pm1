96e95fe4fbb16b053a2ad5f18c274541b675a104
Janardhan-Majumder <janardhan.md03@gmail.com>
2025-11-25 19:49:23 +0600

    input and output practice

---
+++ how_to_print_c++_v2.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // input output stream
+int main()
+{
+    int a = 100;
+    char b = 'A';
+    double c = 10.50;
+
+    std ::cout << "First Printed line" << std::endl
+               << a << " " << b << std::endl
+               << c << std::endl;
+    std::cout << "My favorite number " << a << std::endl;
+
+    return 0;
+}
---
+++ take_input_v3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; // ekhane mean kora hoiyec keyword gulo std theke import kora hoiyece
+int main()
+{
+    int x;
+    char c;
+    double d;
+
+    // cin >> x >> c >> d;
+    // cout << x << " " << c << endl;
+    // cout<<d <<endl;
+
+    // type casting
+    char my_cr = 'a';
+    int ascci = my_cr; // 
+    cout << ascci << endl; // convert integer
+    cout << (int)my_cr << endl;
+
+    int y= 66;
+    cout << (char)y << endl; // convert and print
+
+    return 0;
+}
